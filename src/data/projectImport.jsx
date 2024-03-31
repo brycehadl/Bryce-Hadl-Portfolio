278969fc8b601383fc125129f84fa8a55b27cc6a
Bryce Hadl <brycehadl11@gmail.com>
2024-03-31 18:55:37 -0500

    moving image folder

--- src/data/projectImport.jsx
+++ src/data/projectImport.jsx
@@ -2,28 +2,28 @@ export const project = [
     {
         title: "Dungeonkit",
         description: "Makes a PDF of a dungeons and dragons character sheet that the user can edit.",
-        image: "src/assets/dungeonkit.png",
+        image: "./src/data/dungeonkit.png",
         link: "https://github.com/Chase-Garrett/dungeonkit",
         // website: "https://fetching-baseball.netlify.app/",
     },
     {
         title: "Fetching-Baseball",
         description: "In this project as a team we used API's to create a stats sharing website primarily for Baseball/Football Teams.",
-        image: "src/assets/fetching-baseball.png",
+        image: "./src/data/fetching-baseball.png",
         link: "https://github.com/brycehadl/Map-stats",
         website: "https://fetching-baseball.netlify.app/",
     },
     {
         title: "Social-Network-API",
         description: "The back-end code for a social network API",
-        image: "src/assets/social-network-api.png",
+        image: "./src/data/social-network-api.png",
         link: "https://github.com/brycehadl/Social-Network",
         // website: "https://fetching-baseball.netlify.app/",
     },
     {
         title: "PWA-text-editor",
         description: "Uses webpacks to function even when there is no internet connection.",
-        image: "src/assets/text-editor.png",
+        image: "./src/data/text-editor.png",
         link: "https://github.com/brycehadl/text_editor",
         website: "https://bryce-text-editor-122cc371db37.herokuapp.com/",
         
