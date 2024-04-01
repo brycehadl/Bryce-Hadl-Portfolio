82cb12e91ddb5b1ac93d6342aa90e5947fe22f68
Bryce Hadl <brycehadl11@gmail.com>
2024-03-31 19:13:14 -0500

    photos fixed

--- src/data/projectImport.jsx
+++ src/data/projectImport.jsx
@@ -1,9 +1,13 @@
 import fetching from "../data/fetching-baseball.png";
+import dungeon from "../data/dungeonkit.png";
+import social from "../data/social-network-api.png";
+import texteditor from "../data/text-editor.png";
+
 export const project = [
     {
         title: "Dungeonkit",
         description: "Makes a PDF of a dungeons and dragons character sheet that the user can edit.",
-        image: "./src/data/dungeonkit.png",
+        image: dungeon,
         link: "https://github.com/Chase-Garrett/dungeonkit",
         // website: "https://fetching-baseball.netlify.app/",
     },
@@ -17,14 +21,14 @@ export const project = [
     {
         title: "Social-Network-API",
         description: "The back-end code for a social network API",
-        image: "./src/data/social-network-api.png",
+        image: social,
         link: "https://github.com/brycehadl/Social-Network",
         // website: "https://fetching-baseball.netlify.app/",
     },
     {
         title: "PWA-text-editor",
         description: "Uses webpacks to function even when there is no internet connection.",
-        image: "./src/data/text-editor.png",
+        image: texteditor,
         link: "https://github.com/brycehadl/text_editor",
         website: "https://bryce-text-editor-122cc371db37.herokuapp.com/",
         
